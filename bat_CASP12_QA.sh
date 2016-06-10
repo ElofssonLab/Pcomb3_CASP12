cfea182d73145e7fa73753e273cfa72faa64b8f4
Nanjiang Shu <nanjiang.shu@bils.se>
2016-06-10 09:49:54 +0200

    add a lockfile to prevent the batch script bat_CASP12_QA.sh to run in parallel

--- bat_CASP12_QA.sh
+++ bat_CASP12_QA.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 rundir=`dirname $0`
+rundir=`readlink -f $rundir`
+
 
 cd $rundir
+
+
+lockfile=.casp12_qa.lock
+
+if [ -f $lockfile ]; then
+    echo "lockfile $lockfile exist, exit"
+    exit 1
+fi
+
+date > $lockfile
+
+trap 'rm -f "$lockfile"' INT TERM EXIT
+
 ./clean_bad_predictions.sh
 ./download_prediction.sh
 ./run_QA_proq3.pl
 ./archive_QA.sh
 ./backupto_triolith.sh
+
+rm -f $lockfile
