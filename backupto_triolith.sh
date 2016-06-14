5d42974011b260108fd9865cab346bcaed03465a
Nanjiang Shu <nanjiang.shu@bils.se>
2016-06-14 10:01:03 +0200

    add key with passphrase to the backupto_triolith.sh

--- backupto_triolith.sh
+++ backupto_triolith.sh
@@ -7,8 +7,9 @@ exec_cmd(){
 rundir=`dirname $0`
 
 cd $rundir
-
 OPT1="--exclude=~* --exclude=*~ --exclude=.*.sw[mopn]"
 OPT2="--exclude=.*.lock"
+
+eval `keychain --eval id_rsa_withph` 
 
 exec_cmd "/usr/bin/rsync -auz  ./ $OPT1 $OPT2 x_nansh@triolith.nsc.liu.se:/proj/bioinfo/users/share/CASP12/QA_Pcomb3/"
